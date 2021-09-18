f8a20bb8dcb647409bf42d22d7ad7c43273a2fd2
Violet <violet@geeklint.com>
2021-09-18 11:00:53 -0400

    add license, crate metadata

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+/* SPDX-License-Identifier: (Apache-2.0 OR MIT OR Zlib) */
+/* Copyright © 2021 Violet Leonard */
+
 //! This crate provides abstractions to type-erase various lists
 //! (Vecs and slices).
 //!
--- src/vec.rs
+++ src/vec.rs
@@ -1,3 +1,6 @@
+/* SPDX-License-Identifier: (Apache-2.0 OR MIT OR Zlib) */
+/* Copyright © 2021 Violet Leonard */
+
 #![allow(clippy::module_name_repetitions)]
 
 use core::{
